7751364242e487453fa2fc02399036dbfd856940
saisreedhar2001 <saisreedhar2001@gmail.com>
2023-05-05 10:51:08 +0530

    commit3

--- pr2.py
+++ pr2.py
@@ -2,25 +2,14 @@ import numpy as np
 import pickle
 import streamlit as st
 from PIL import Image
+import random
 
 def load_model(modelfile):
   loaded_model = pickle.load(open('sepsis.pkl', 'rb'))
   return loaded_model
 
 
-# creating a function for Prediction
-def diabetes_prediction(input_data):
-    # convert input data to a NumPy array of type float
-    input_data_as_numpy_array = np.asarray(input_data, dtype=float)
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
-    # make the prediction using the loaded model
-    prediction = loaded_model.predict(input_data_reshaped)
-    # return the predicted class
-    if prediction[0] == 0:
-        return 'Non sepsis'
-    else:
-        return 'Sepsis'
+
 def main():
     # giving a title
 
@@ -35,7 +24,7 @@ def main():
     with col1: 
         with st.expander(" ℹ️ Information", expanded=True):
             st.write("""
-             Sepsis is a potentially life-threatening condition that occurs when the body's response to an infection damages
+            Sepsis is a potentially life-threatening condition that occurs when the body's response to an infection damages
              its own tissues and organs. It is a systemic inflammatory response syndrome (SIRS) caused by an infection, 
              which can be bacterial, viral, fungal, or parasitic. The immune system's response to the infection can cause 
              widespread inflammation throughout the body, leading to organ damage and failure. Sepsis is a medical 
@@ -47,44 +36,43 @@ def main():
         '''
         ## How does it work ❓ 
         Fill all the parameters and the machine learning model will predict whether the patient affected with sepsis or not.
-        
         '''
+      
+        col1.write("Model Used: Light gradient Boosting Machine Classifier")
+        col1.write("Developer: C.Naga Sai Sreedhar")
 
     with col2:
-        st.subheader("  Predict Sepsis using machine learning model 👨‍⚕️")
-        HR = st.number_input('Heart rate (beats per minute)')
-        o2sat = st.number_input('Pulse oximetry (%)')
-        SBP = st.number_input('Systolic BP (mm Hg)')
-        MAP = st.number_input('Mean arterial pressure (mm Hg)')
-        DBP = st.number_input('Diastolic BP (mm Hg)')
-        Resp = st.number_input('Respiration rate (breaths per minute)')
-        Age = st.number_input('AGE')
-        HospAdmTime = st.number_input('Hours between hospital admit and ICU admit')
-        ICULOS = st.number_input('ICU length-of-stay (hours since ICU admit)')
+        st.subheader(" Predict Sepsis using machine learning model 👨‍⚕️")
+        HR = st.number_input('Heart rate (beats per minute)',key=None)
+        o2sat = st.number_input('Pulse oximetry (%)',key=None)
+        SBP = st.number_input('Systolic BP (mm Hg)',key=None)
+        MAP = st.number_input('Mean arterial pressure (mm Hg)',key=None)
+        DBP = st.number_input('Diastolic BP (mm Hg)',key=None)
+        Resp = st.number_input('Respiration rate (breaths per minute)',key=None)
+        Age = st.number_input('AGE',key=None)
+        HospAdmTime = st.number_input('Hours between hospital admit and ICU admit',key=None)
+        ICULOS = st.number_input('ICU length-of-stay (hours since ICU admit)',key=None)
 
         feature_list = [HR, o2sat, SBP, MAP, DBP, Resp, Age, HospAdmTime, ICULOS]
-        single_pred = np.array(feature_list).reshape(1,-1)
+        df2 = [[109,91,132,96.67,0,24,83.14,-0.03,16],[0,0,0,0,0,0,83.14,-0.03,31],[88,97,129,85,63,13,43.43,-0.02,90],[80,94,91,61,63,22,44.9,-44.37,111],[87,98,150,77,53,18,63.25,-3.85,34]]
+      
         
         if st.button('Predict'):
-
-            loaded_model = load_model('model.pkl')
-            prediction = loaded_model.predict(single_pred)
+            loaded_model = pickle.load(open('sepsis.pkl', 'rb'))
+            prediction = loaded_model.predict([feature_list])
             col1.write('''
 		    ## Results 🔍 
 		    ''')
-            if prediction.item() == 0:
-                col1.success(f"sepsis not identified")
+            col1.write(feature_list)
+            
+            if prediction.item ==0: 
+                col1.success("Sepsis not identified 😊")
             else:
-                col1.success(f"sepsis identified")
+                col1.success("Sepsis identified 😞")
+                
             #col1.success(f"{prediction.item()} are recommended by the A.I for your farm.")
-      #code for html ☘️ 🌾 🌳 👨‍🌾  🍃
-
-   
-
-
-
-
-
+            #code for html ☘️ 🌾 🌳 👨‍🌾  🍃
+        
 
 
 if __name__ == '__main__':
